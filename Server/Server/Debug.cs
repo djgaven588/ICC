bfec182885e1e30e8909026484ecece8e0eca811
Cj Herman <cjherman588@gmail.com>
2019-03-27 15:25:46 -0700

    Moved log thing to debug class

---
+++ Server/Server/Debug.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    public static class Debug
+    {
+        public static void Log(string message, string from)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(from + ":");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine(message);
+        }
+    }
+}
--- Server/Server/Program.cs
+++ Server/Server/Program.cs
@@ -80,31 +80,31 @@ namespace Server
         {
             if(args == null || args.Length == 0)
             {
-                args = new string[] { "Welcome! This server is in debug mode...", "1337"};
+                args = new string[] { "Welcome! This server is in debug mode..."};
             }
             Console.Title = "ICC - Console Server";
             if (args == null || args.Length == 0 || args[0] == null)
             {
-                Log("A daily message was not specified in the program arguments, it is recommended to set this up. Using default daily message...", "Daily Message");
+                Debug.Log("A daily message was not specified in the program arguments, it is recommended to set this up. Using default daily message...", "Daily Message");
                 dailyMsg = "Welcome to the server! This server does not have a daily message set.\nIf you are the server owner, make sure to run the server with the first parameter as the daily message!";
                 passwordProtected = false;
-                Log("This server is not password protected. This is fine if you want anyone and everyone to connect, but can be changed to add security. This can be changed by editing the second program argument", "Password Protection");
+                Debug.Log("This server is not password protected. This is fine if you want anyone and everyone to connect, but can be changed to add security. This can be changed by editing the second program argument", "Password Protection");
             }
             else
             {
-                Log("The daily message is: " + args[0] + ". You can change this by changing the first program argument when starting the server.", "Daily Message");
+                Debug.Log("The daily message is: " + args[0] + ". You can change this by changing the first program argument when starting the server.", "Daily Message");
                 dailyMsg = args[0];
 
                 if (args.Length > 1 && args[1] != null)
                 {
                     passwordProtected = true;
                     password = args[1];
-                    Log("This server is password protected. The password is " + password + ". You can change this by changing the second program argument when starting the server.", "Password Protection");
+                    Debug.Log("This server is password protected. The password is " + password + ". You can change this by changing the second program argument when starting the server.", "Password Protection");
                 }
                 else
                 {
                     passwordProtected = false;
-                    Log("This server is not password protected. This is fine if you want anyone and everyone to connect, but can be changed to add security. This can be changed by editing the second program argument", "Password Protection");
+                    Debug.Log("This server is not password protected. This is fine if you want anyone and everyone to connect, but can be changed to add security. This can be changed by editing the second program argument", "Password Protection");
                 }
             }
             new Program().Setup();
@@ -118,7 +118,7 @@ namespace Server
 
             server = new Telepathy.Server();
             server.Start(9999);
-            Log("Server online! Connect to it using the port 9999\nThe local ip of the server is: " + GetLocalIPAddress()
+            Debug.Log("Server online! Connect to it using the port 9999\nThe local ip of the server is: " + GetLocalIPAddress()
                 + "\nIf you want this server to be accessed by the outside world, port forward port 9999 and give users your public address." 
                 + "\nNOTE: You, the server host, are responsible for anything that happens because of port forwarding. Be careful.", "Server Startup");
             Loop();
@@ -138,7 +138,7 @@ namespace Server
                         case Telepathy.EventType.Connected:
                             connections.Add(msg.connectionId, new ConnectionData(msg.connectionId));
                             connections[msg.connectionId].SendMessage(passwordProtected ? "passwordProtected" : "");
-                            Log("A user has connected, they have been notified of the servers password protection status.", "New Connection");
+                            Debug.Log("A user has connected, they have been notified of the servers password protection status.", "New Connection");
                             
                             break;
                         case Telepathy.EventType.Data:
@@ -151,31 +151,31 @@ namespace Server
                                 {
                                     if (msgContents == password)
                                     {
-                                        Log("A user has established communication. Waiting for a username...", "User Login Success");
+                                        Debug.Log("A user has established communication. Waiting for a username...", "User Login Success");
                                         connection.currentStage = ConnectionData.ConnectionStage.AwaitingNickname;
                                         connection.SendMessage("V");
                                         connection.SendMessage("Welcome to the server client! Please send your nickname.");
                                     }
                                     else
                                     {
-                                        Log("A user tried to connect with a password which didn't match. Notifying user.", "User Login Failed");
+                                        Debug.Log("A user tried to connect with a password which didn't match. Notifying user.", "User Login Failed");
                                         connection.SendMessage("Your password was invalid. Please try again.");
                                     }
                                 }
                                 else
                                 {
-                                    Log("A user has established communication. Waiting for a username...", "User Connected");
+                                    Debug.Log("A user has established communication. Waiting for a username...", "User Connected");
                                     connection.currentStage = ConnectionData.ConnectionStage.AwaitingNickname;
                                     connection.SendMessage("Welcome to the server client! Please send your nickname.");
                                 }
                             }
                             else if (connection.currentStage == ConnectionData.ConnectionStage.AwaitingNickname)
                             {
-                                Log("A user has requested to use the username '" + msgContents + "'", "Nickname");
+                                Debug.Log("A user has requested to use the username '" + msgContents + "'", "Nickname");
                                 bool result = connection.SetNickname(msgContents);
                                 if (result)
                                 {
-                                    Log("The username '" + msgContents + "' was available and valid. Welcome " + msgContents + "!", "Nickname");
+                                    Debug.Log("The username '" + msgContents + "' was available and valid. Welcome " + msgContents + "!", "Nickname");
                                     connection.currentStage = ConnectionData.ConnectionStage.ConnectionEstablished;
                                     connection.SendMessage("Welcome " + connection.nickname + "!");
                                     connection.SendMessage("V");
@@ -184,13 +184,13 @@ namespace Server
                                 }
                                 else
                                 {
-                                    Log("The username '" + msgContents + "' was not available or not valid. The user was notified.", "Nickname");
+                                    Debug.Log("The username '" + msgContents + "' was not available or not valid. The user was notified.", "Nickname");
                                     connection.SendMessage("Your username is either too long, too short, or is already in use!\nMake sure your username is unique, greather than 3 characters, and less than 16 characters!");
                                 }
                             }
                             else
                             {
-                                Log(connection.nickname + " said: " + msgContents, "User Message");
+                                Debug.Log(connection.nickname + " said: " + msgContents, "User Message");
                                 BroadcastMsg(connection.nickname + ": " + msgContents);
                             }
 
@@ -200,7 +200,7 @@ namespace Server
                         case Telepathy.EventType.Disconnected:
                             ConnectionData connectData = connections[msg.connectionId];
                             connectData.RemoveConnection();
-                            Log((connectData.nickname != null ? "User " + connectData.nickname : "Unknown User") + " Disconnected", "Disconnection");
+                            Debug.Log((connectData.nickname != null ? "User " + connectData.nickname : "Unknown User") + " Disconnected", "Disconnection");
                             if (connectData.nickname != null)
                             {
                                 BroadcastMsg("A user who went by " + connectData.nickname + ", has left the chat... They shall me missed!");
@@ -241,14 +241,6 @@ namespace Server
         public void LogTelepathy(string message)
         {
             //Not setup as it is messy.
-        }
-
-        public static void Log(string message, string from)
-        {
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine(from + ":");
-            Console.ForegroundColor = ConsoleColor.Gray;
-            Console.WriteLine(message);
         }
     }
 }
