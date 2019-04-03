a414949ec4937be1504b1d05603704778cac1971
Cj Herman <cjherman588@gmail.com>
2019-04-03 16:32:57 -0700

    Working on commands. The system allows for you to create commands, and register them. You currently have to be a little careful with commands, as you are given an array of arguments which may or may not contain what you require.

--- Server/Server/CustomEventHandler.cs
+++ Server/Server/CustomEventHandler.cs
@@ -8,7 +8,7 @@ namespace Server
 {
     public class CustomEventHandler : EventHandler
     {
-        private Dictionary<string, Action<string[], ConnectionData>> commands = new Dictionary<string, Action<string[], ConnectionData>>(); 
+        private Dictionary<string, Command> commands = new Dictionary<string, Command>(); 
 
         public override void OnMessageReceived(string msgContents, ref ConnectionData connection)
         {
@@ -24,20 +24,56 @@ namespace Server
         private void HandleCommand(string command, ref ConnectionData connection)
         {
             string[] segments = command.Split(' ');
-            if (commands.TryGetValue(segments[0].Substring(1), out Action<string[], ConnectionData> action))
+            if (commands.TryGetValue(segments[0].Substring(1), out Command action))
             {
-                action.Invoke(segments, connection);
+                connection = action(segments, connection);
+            }
+            else
+            {
+                connection.SendMessage($"The command '{segments[0]}' is not an available command. Use /help to help you get started with commands.");
             }
         }
 
         public CustomEventHandler()
         {
-            commands.Add("nick", (string[] args, ConnectionData connection) => { ChangeNickname(args, connection); });
+            Command cmdNick = ChangeNicknameMethod;
+            Command cmdHelp = HelpMethod;
+            commands.Add("nick", cmdNick);
+            commands.Add("help", cmdHelp);
         }
 
-        private void ChangeNickname(string[] args, ConnectionData connection)
-        {
+        private delegate ConnectionData Command(string[] args, ConnectionData connection);
 
+        private ConnectionData ChangeNicknameMethod(string[] args, ConnectionData connection)
+        {
+            if (args.Length <= 1)
+            {
+                connection.SendMessage("The 'nick' command requires you to specify what your new nickname is, please try again with the correct parameters.");
+                return connection;
+            }
+
+            bool result = connection.SetNickname(args[1]);
+            if (result)
+            {
+                connection.SendMessage($"The nickname '{args[1]}' was available and valid! Your new nickname is set!");
+            }
+            else
+            {
+                connection.SendMessage($"The nickname '{args[1]}' was not availible, or may not have been valid. Please try again with another nickname.");
+            }
+            return connection;
+        }
+
+        private ConnectionData HelpMethod(string[] args, ConnectionData connection)
+        {
+            connection.SendMessage("--Help Center--");
+            connection.SendMessage("");
+            connection.SendMessage("-Available Commands-");
+            foreach (string item in commands.Keys)
+            {
+                connection.SendMessage("/" + item);
+            }
+            return connection;
         }
     }
 }
